8ea06580016ff4ebb15068ad17ca8fd61c0814e3
VTsantilis <vtsantilis@gmail.com>
2015-09-11 12:07:11 +0300

    add instruction for old android (reverted from commit ea4322e2100a26d768703f3082f1d5e2d5e1df3a)

--- sites/all/themes/water/templates/html.tpl.php
+++ sites/all/themes/water/templates/html.tpl.php
@@ -8,7 +8,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="description" content="">
-<meta name="viewport" content="width=device-width, initial-scale=1, target-densitydpi=device-dpi">
+<meta name="viewport" content="width=device-width, initial-scale=1">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
