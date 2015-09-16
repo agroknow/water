c7eac2e246c18a94bae17f062dde98fbed5e8ce1
VTsantilis <vtsantilis@gmail.com>
2015-09-16 20:55:17 +0300

    add files for partners view

---
+++ sites/all/themes/water/views/views-view-unformatted--partners.tpl.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php
+$rowsnum = count($rows);
+$mod4 = $rowsnum % 4;
+$mod3 = $rowsnum % 3;
+$break = 0;
+if ($rowsnum <= 4) {
+    $cols = $rowsnum;
+} else {
+    if($mod4 == 0){
+	$cols = 4;
+    } else {
+	if($mod3 == 0) {
+	    $cols = 3;
+	} else {
+	    if($mod4 == 1) {
+		$cols = 3;
+		$break = $rowsnum - $mod3;
+		$breakcols = $mod3;
+	    } else {
+		$cols = 4;
+		$break = $rowsnum - $mod4;
+		$breakcols = $mod4;
+	    }
+	}
+	
+    }
+}
+?>
+<?php if (!empty($title)): ?>
+    <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<div class="cols">    
+<?php foreach ($rows as $id => $row): ?>
+    <div class="col colspan<?php if($break && $id >= $break) { echo 12/$breakcols; } else { echo 12/$cols; } ?>">
+    <?php print $row; ?>
+    </div>
+<?php endforeach; ?>
+</div>
