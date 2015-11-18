c62c544eab52bd535d94a03f051afd72bf9591fb
VTsantilis <vtsantilis@gmail.com>
2015-11-18 13:01:50 +0200

    change menu Glossary to Newsletter

--- sites/all/themes/water/templates/block--search--form.tpl.php
+++ sites/all/themes/water/templates/block--search--form.tpl.php
@@ -1,6 +1,6 @@
 <section class="search clearfix" data-theme-tpl="block--search--form">
     <?php print $content ?>
-    <a href="#">A-z Glossary</a>
+    <a href="<?php print url('newsletters') ?>">Newsletter</a>
     <a href="<?php print base_path(); ?>search/node">Search</a>
 <a href="<?php print base_path(); ?>booktree">Book Contents</a>
 </section>
